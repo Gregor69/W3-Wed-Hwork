9097125749bf6d7b05d4341b58ea2f821e1f8939
Gregor Simpson <catkelly@Catrionas-MacBook-Pro.local>
2021-01-20 21:37:04 +0000

    updated homework

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template
 from app.models.order_list import orders
 
-@app.route('/')
+@app.route('/orders')
 def index():
     return render_template('index.html', title= 'Super Sarnies', orders=orders)
 
